c62d8f47eea1ea24cccc855e6a52d495b8aed92e
kachilala <mac@macs-MacBook-Pro.local>
2019-11-28 18:43:40 +0100

    add route with installation of react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,19 +1,20 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
-import './App.css';
 
+import CreateTodo from "./components/create-todo.component";
+import EditTodo from "./components/edit-todo.component";
+import TodosList from "./components/todos-list.component";
 function App() {
   return (
     <Router>
       <div className="container">
         <h2>MERN-Stack Todo App</h2>
-      </div>
-
       <Route path="/" exact component={TodosList} />
       <Route path="/edit/:id" component={EditTodo} />
       <Route path="/create" component={CreateTodo} />
-    </Router>
+      </div>
+      </Router>
   );
 }
 
