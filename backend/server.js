1330e1f49dd8f582eff049984b3c808a7e33a3a1
kachilala <mac@macs-MacBook-Pro.local>
2019-11-28 20:28:14 +0100

    add form for creating todo and start backend development using node

---
+++ backend/server.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const mongoose = require('mongoose');
+const PORT = 4000;
+
+app.use(cors());
+app.use(bodyParser.json());
